fd47c3cf8ccabef4641241483c6aa7ef1074ccc0
Bruno Gonçalves <codyzzd@gmail.com>
2020-05-28 21:58:01 -0300

    muda a cor da propriedade da div

--- js.js
+++ js.js
@@ -1,29 +1,36 @@
-window.addEventListener('load', () => {
+/*window.addEventListener('load', () => {
   console.log("ready!");
   //alert("ready");
-});
-
-//guardando a cor C em variavel
-var color_ciano = document.getElementById('ciano');
-//guardando a cor M em variavel
-var color_magenta = document.getElementById('magenta');
-//guardando a cor Y em variavel
-var color_yellow = document.getElementById('yellow');
-//guardando a cor B em variavel
-var color_black = document.getElementById('black');
+});*/
 
 //quando mudar o range coloca o valor no input
-document.getElementById('muda_cores').onchange = function () {
+document.getElementById('muda_cores').oninput = function () {
   campo_valor = event.target;
 
   if (campo_valor.type == "range") {
     //se mexeu no range atualiza no range
     document.getElementById(campo_valor.name + "_valor").value = campo_valor.value;
+
   } else {
     //se mexeu no text atualiza aqui
     document.getElementById("range_" + campo_valor.name).value = campo_valor.value;
   }
-  //mostra tipo do campo + valor do campo + id do campo
-  console.log(campo_valor.type + " " + campo_valor.value + " " + campo_valor.id);
 
+  //pega as cores finais dos campos
+  ciano_final = document.getElementById('ciano_valor').value;
+  magenta_final = document.getElementById('magenta_valor').value;
+  yellow_final = document.getElementById('yellow_valor').value;
+  black_final = document.getElementById('black_valor').value;
+
+  //poem a cor no input e guarda variavel
+  bg = document.getElementById("cmykcss").value = "cmyk(" + ciano_final + "%, " + magenta_final + "%, " + yellow_final + "%, " + black_final + "%)"
+
+  //muda a cor de fundo da div baseado na variavel
+  const divcor = document.querySelector("#exibir_cor");
+  divcor.setAttribute('style', "width:300px;height:300px;background: " + bg);
+
+  console.log(bg)
+
+  //mostra tipo do campo + valor do campo + id do campo
+  //console.log(campo_valor.type + " " + campo_valor.value + " " + campo_valor.id);
 }
